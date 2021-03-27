0281a5091b3b1be4c403ab7e0bf0f939d514a275
haneenzyad98 <haneenzyad98@gmail.com>
2021-03-28 02:39:01 +0300

    lab

--- js/app.js
+++ js/app.js
@@ -1,61 +1,110 @@
 'use strict';
-let allimg=[];
-let allkey=[];
+let allimg = [];
+let keyWord = [];
 
 
-function imge(image_url,title,description,keyword,horns){
-    this.image_url=image_url;
-    this.title=title;
-    this.description=description;
-    this.keyword=keyword;
-    this.horns=horns;
+function Imge(img_url, title, description, keyword, horns) {
+    this.img_url = img_url;
+    this.title = title;
+    this.description = description;
+    this.keyword = keyword;
+    this.horns = horns;
+    this.id = index;
     allimg.push(this)
-    allkey.push(this.keyword)
+
 }
- console.log(allimg);
- console.log(allkey);
- console.log('test');
- 
-imge.prototype.render=function(){ 
-    let imgsec=$('#photo-template').clone();
-    $('main').append(imgsec);
-    $('select').append(`<option value='${this.keyword}'>${this.keyword}</option>`);
-
-    imgsec.find('h2').text(this.title);
-    imgsec.find('img').attr('src', this.image_url);
-    imgsec.find('p').text(this.description)
-    imgsec.removeAttr('id');
-
-    
+let index = 0;
+let x;
+Imge.prototype.renderAll = function () {
+    x = $('#photo-template').clone();
+    $('main').append(x);
+    x.find('h2').text(this.title);
+    x.find('img').attr('src', this.img_url);
+    x.find('p').text(this.description);
+    x.removeAttr('id');
+    x.attr('id', this.id);
+    index++;
 }
-// function castvote() { 
-//     var sel = document.getElementById("select");
-//  console.log(sel);
-//     alert(sel.options[sel.selectedIndex].value); 
-  
-//  }
-//  castvote();
+keyWord.push(this.keyword)
+console.log(keyWord);
+Imge.prototype.filloption = function () {
+    for (let i = 0; i < keyWord.length; i++) {
+        let newOption = $('<option></option>');
 
-function getimgData() {
-    console.log('test2');
+        if ($('option').val() != keyWord[i]) {
+            $('#select').append(newOption);
+            newOption.text(this.keyword);
+        }
+    }
+}
+
+function renderSelect() {
+    $('#select').on('click', function () {
+        for (let i = 0; i < index; i++) {
+            if (allimg[i].keyword == $('#select').val()) {
+                $('#' + i).show();
+            } else {
+                if ($('#select').val() == 'alloption') {
+                    $('#' + i).show();
+                } else {
+                    $('#' + i).hide();
+                }
+                
+            }
+        }
+    })
+}
+
+
+function getData() {
     const ajaxSettings = {
         method: 'get',
         dataType: 'json'
+    };
+    $.ajax('data.json', ajaxSettings).then(data => {
+        data.forEach(element => {
+            let newimg = new Imge(element.image_url, element.title, element.description, element.keyword, element.horns);
+             newimg.renderAll();
+            newimg.filloption();
+        });
     }
-    $.ajax('data.json', ajaxSettings).then(data =>{
-    console.log("we got the data!!")
-    data.forEach(element => {
-        let imgobj=new imge(element.image_url,element.title,element.description,element.keyword,element.horns)
-        
-    imgobj.render();
-    
 
-  
-    });
-})
+    )
+}
+function getData2() {
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    };
+    $.ajax('data2.json', ajaxSettings).then(data => {
+        data.forEach(element => {
+            let newimg = new Imge(element.image_url, element.title, element.description, element.keyword, element.horns);
+            newimg.renderAll();
+            newimg.filloption();
+        });
+    }
 
-
+    )
 }
 
-$('document').ready(getimgData);
+let pageOneSelector = () => {
+    $('photo-template').hide();
+    getData();
+    renderSelect();
+    
+};
+
+
+let pageTwoSelector = () => {
+    $('photo-template').hide();
+ $('option').remove();
+ renderSelect();
+    getData2();
+};
+
+$('#pageOne').on('click', pageOneSelector);
+$('#pageTwo').on('click', pageTwoSelector);
+
+$('document').ready(getData);
+renderSelect();
 
